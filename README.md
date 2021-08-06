ee2c1a9bab607de3b7ee2b6658266a9af9aa3695
Vauff <mctehkitti@gmail.com>
2021-08-06 17:30:20 -0400

    add code

---
+++ README.md
@@ -0,0 +1,3 @@
+# stripper-case-action
+
+A GitHub action to verify that stripper filenames are all lowercase in the [gflclan-cs-go-ze/ZE-Configs](https://github.com/gflclan-cs-go-ze/ZE-Configs) repository.
---
+++ main.js
@@ -0,0 +1,29 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require("fs");
+
+try
+{
+	const stripperPath = core.getInput("stripper-path");
+	const filenames = fs.readdirSync(stripperPath);
+	let failCount = 0;
+
+	console.log("Verifying all stripper configs are lower case...");
+
+	filenames.forEach((file) => {
+		if (file.toLowerCase() !== file)
+		{
+			failCount++;
+			console.log("ERROR: ${file} is not in lower case");
+		}
+	});
+
+	if (failCount === 0)
+		console.log("All stripper config filenames successfully verified as lower case!");
+	else
+		core.setFailed("${failCount} stripper config filenames failed the lower case check!");
+}
+catch (error)
+{
+	core.setFailed("An unexpected error occured: ${error.message}");
+}
